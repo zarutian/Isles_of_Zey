4b0af2e69a177b1e30308984eff503172557b7c2
Zarutian/FabLabReyk <zarutian@gmail.com>
2014-05-12 15:08:08 +0000

    Init commit

---
+++ SHA2.lua
@@ -0,0 +1,3 @@
+package.preload["SHA256"] = function (module_name)
+ 
+end
---
+++ main.lua
@@ -0,0 +1,11 @@
+love.load = function ()
+  print(bit32);
+end
+love.draw = function () end
+love.update = function (deltatime) end
+love.mousepressed = function (x, y, button) end
+love.mousereleased = function (x, y, button) end
+love.keypressed = function (key) end
+love.keyreleased = function (key) end
+love.focus = function (gotFocus) end
+love.quit = function () end
